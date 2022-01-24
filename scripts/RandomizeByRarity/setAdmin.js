b91bb51728c6f2f496ad7fe5ff3e41f29e059172
Quang Dinh <ngocquangbb@gmail.com>
2022-01-24 17:32:56 +0700

    setAdmin

--- scripts/RandomizeByRarity/setAdmin.js
+++ scripts/RandomizeByRarity/setAdmin.js
@@ -17,8 +17,9 @@ async function main() {
   // TODO: add real admin
   const admins = [
     // "0xAE51701F3eB7b897eB6EE5ecdf35c4fEE29BFAe6",
-    "0x0c1954CEB2227e3C5E6155B40fd929C1fF64F5f5",
-    "0xA8f68bB8d525f5874df9202c63C1f02eeC3dFE1f",
+    // "0x0c1954CEB2227e3C5E6155B40fd929C1fF64F5f5",
+    // "0xA8f68bB8d525f5874df9202c63C1f02eeC3dFE1f",
+    "0xE8AE51B507CeB672712E99588a8b3Aa991A05420",
   ];
 
   for (var i=0;i<admins.length;i++) {
